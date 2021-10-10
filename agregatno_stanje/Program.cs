f5150fa68e39f5eb32c0906f09315ca458b0642f
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-10 15:52:11 +0200

    Add files via upload

---
+++ agregatno_stanje/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace agregatno_stanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a < 0)
+            {
+                Console.WriteLine("cvrsto");
+            }
+            else if (a > 0 & a < 100)
+            {
+                Console.WriteLine("tecno");
+            }
+            else if (a > 100)
+            {
+                Console.WriteLine("gasovito");
+            }
+        }
+    }
+}
